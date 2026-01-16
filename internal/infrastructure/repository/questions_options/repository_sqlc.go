8230be8b59d57c5e5c66934889019c8b6f552c4f
felipe panosso <panosso@localhost.localdomain>
2026-01-16 15:34:06 -0300

    Refactoring: In the SQLC repository, two methods of QuestionOption were refactored. The first method, which creates a question option, previously only returned an error; now it returns the entity and the error. The second method, ExistsQuestionOptionByValue, which checks if an option exists based on its value, incorrectly returns the entity and an error. It should only return a bool and the error if it occurs. Both have been corrected.

--- internal/infrastructure/repository/questions_options/repository_sqlc.go
+++ internal/infrastructure/repository/questions_options/repository_sqlc.go
@@ -5,7 +5,6 @@ import (
 	"database/sql"
 	"errors"
 
-	"github.com/FelipePn10/panossoerp/internal/domain/product/repository"
 	"github.com/FelipePn10/panossoerp/internal/domain/questions_options/entity"
 	"github.com/FelipePn10/panossoerp/internal/infrastructure/database/sqlc"
 )
@@ -13,30 +12,38 @@ import (
 func (r *repositoryQuestionOptionsSQLC) Save(
 	ctx context.Context,
 	qstops *entity.QuestionsOptions,
-) error {
-	_, err := r.q.CreateQuestionOption(ctx, sqlc.CreateQuestionOptionParams{
+) (*entity.QuestionsOptions, error) {
+	dbQuestionOption, err := r.q.CreateQuestionOption(ctx, sqlc.CreateQuestionOptionParams{
 		Value:      qstops.Value,
 		CreatedBy:  qstops.CreatedBy,
 		QuestionID: qstops.QuestionId,
 	})
-	return err
-}
-
-func (r *repositoryQuestionOptionsSQLC) ExistsQuestionOptionByValue(
-	ctx context.Context,
-	value string,
-) (*entity.QuestionsOptions, error) {
-	dbQuestionOption, err := r.q.ExistsQuestionOptionByValue(ctx, value)
 	if err != nil {
 		if errors.Is(err, sql.ErrNoRows) {
-			return nil, repository.ErrNotFound
+			return nil, err
 		}
+		return nil, err
 	}
+
 	return &entity.QuestionsOptions{
 		QuestionId: dbQuestionOption.ID,
 		CreatedBy:  dbQuestionOption.CreatedBy,
 		Value:      dbQuestionOption.Value,
 	}, nil
+}
+
+func (r *repositoryQuestionOptionsSQLC) ExistsQuestionOptionByValue(
+	ctx context.Context,
+	value string,
+) (bool, error) {
+	_, err := r.q.ExistsQuestionOptionByValue(ctx, value)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return false, err
+		}
+		return false, err
+	}
+	return true, nil
 }
 
 func (r *repositoryQuestionOptionsSQLC) Delete(
